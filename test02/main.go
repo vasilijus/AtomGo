2623672797e617fe4069d020d5fdd2e3e9902b6a
Sergio <vasiliokas@gmail.com>
2021-11-21 00:39:39 +0000

    quick

--- test02/main.go
+++ test02/main.go
@@ -5,12 +5,22 @@ import (
   "net/http"
 )
 
+// port := ":8080"
+
 func home_page(page http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(page, "Go is easssy")
 }
+func about_page(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "about us")
+}
+
+func handleRequest() {
+  fmt.Println("start server on 8080")
+  http.HandleFunc("/", home_page)
+  http.HandleFunc("/about/", about_page)
+  http.ListenAndServe(":8080", nil) // 2 parameter(settings)
+}
 
 func main() {
-  fmt.Println("start serv")
-  http.HandleFunc("/", home_page)
-  http.ListenAndServe(":8080", nil) // 2 parameter(settings)
+  handleRequest()
 }
