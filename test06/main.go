b3dee7e49163936392ffd79204416af949a4518a
Sergio <vasiliokas@gmail.com>
2021-11-24 18:59:02 +0000

    attach templ to the http request

--- test06/main.go
+++ test06/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "net/http"
+  "html/template"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+  temp, err :=template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
+
+  if err != nil {
+    fmt.Fprintf(w, err.Error()) // display on page
+  }
+
+  temp.ExecuteTemplate(w, "index", nil) // page / index / parameters
+}
+
+func handleFunc() {
+  http.HandleFunc("/", index)
+  http.ListenAndServe(":8080", nil)
+}
 
 func main() {
-  fmt.Println("hello pal, less 6")
+  handleFunc()
 }
