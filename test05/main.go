f5ecc64fbe40313454114b171d8b86940cc5e51e
Sergio <vasiliokas@gmail.com>
2021-11-24 18:32:22 +0000

    User struct added

--- test05/main.go
+++ test05/main.go
@@ -7,6 +7,11 @@ import (
   _ "github.com/go-sql-driver/mysql"
 )
 
+type User struct {
+  Name string `json:"name"`
+  Age uint16 `json:"age"`
+}
+
 func main() {
   fmt.Println("Working with MySQL")
 
@@ -15,16 +20,18 @@ func main() {
   if err != nil {
     panic(err)
   }
-  
+
   defer db.Close()
 
   // insert data
-  insert, err := db.Query("INSERT INTO `users` (`name`, `age`) VALUES('Sergio', 30)")
-  if err != nil {
-    panic(err)
-  }
+  // insert, err := db.Query("INSERT INTO `users` (`name`, `age`) VALUES('Sergio', 30)")
+  // if err != nil {
+  //   panic(err)
+  // }
+  // defer insert.Close()   
 
-  defer insert.Close()   
+  // Select data from db
+
 
   fmt.Println("Connected to MySQL")
   // db.SetConnMaxLifetime(5)
