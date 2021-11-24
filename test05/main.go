6393dc2da454fcfa985eee2709828742b9f3de78
Sergio <vasiliokas@gmail.com>
2021-11-24 16:47:58 +0000

    allow inserting data to db

--- test05/main.go
+++ test05/main.go
@@ -1,6 +1,32 @@
 package main
 
-import ( "fmt", "database/sql" )
+import ( 
+  "fmt"
+  "database/sql" 
+
+  _ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
   fmt.Println("Working with MySQL")
+
+  db, err := sql.Open("mysql", "test:password@/test_go_db") 
+  // dbType / login, pass, host, port, database 
+  if err != nil {
+    panic(err)
+  }
+  
+  defer db.Close()
+
+  // insert data
+  insert, err := db.Query("INSERT INTO `users` (`name`, `age`) VALUES('Sergio', 30)")
+  if err != nil {
+    panic(err)
+  }
+
+  defer insert.Close()   
+
+  fmt.Println("Connected to MySQL")
+  // db.SetConnMaxLifetime(5)
+  // db.SetMaxOpenConns(10)
 }
