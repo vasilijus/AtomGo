3ce953805cd387c1f7d7f2e8b4e198552a16aad1
Sergio <vasiliokas@gmail.com>
2021-11-26 16:42:21 +0000

    create page form, func

--- test07/main.go
+++ test07/main.go
@@ -18,10 +18,26 @@ func index(w http.ResponseWriter, r *http.Request) {
   temp.ExecuteTemplate(w, "index", nil) // page / index / parameters
 }
 
+func create(w http.ResponseWriter, r *http.Request) {
+  temp, err :=template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html")
+
+  if err != nil {
+    fmt.Fprintf(w, err.Error()) // display on page
+  }
+
+  temp.ExecuteTemplate(w, "create", nil) // page / index / parameters
+}
+func save_article(w http.ResponseWriter, *r Request) {
+  title := r.FormValue("title")
+  
+}
+
 func handleFunc() {
   // allow first to add static files ( css/js/images)
   http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/") ) ) )
   http.HandleFunc("/", index)
+  http.HandleFunc("/create", create)
+  http.HandleFunc("/save_article", save_article)
   http.ListenAndServe(":8080", nil)
 }
 
